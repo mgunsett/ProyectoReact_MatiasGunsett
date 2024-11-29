a98aebd5fa432bcfde77c0c1f2a04078558cf7ca
Matias Gunsett <matiasgunsett@gmail.com>
2024-11-29 07:27:51 -0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -21,17 +21,38 @@ export const Footer = () => {
             </Box>
             <Text>© 2024 BeReal. Todos los Derechos Reservados.</Text>
             <Stack direction={'row'} spacing={6}>
-                <Button label={'Twitter'}>
+                <Button 
+                    label={'Twitter'} 
+                    boxSize={'35px'}
+                    _hover={{
+                        bg: 'linear-gradient(40deg, #1DA1F2, #1DA1F2, #1DA1F2, #1DA1F2, #1DA1F2, #1DA1F2)',
+                        color: 'white'
+                    }}
+                >
                     <Link to={'https://twitter.com/'} target='_blank'>
                         <FaTwitter />
                     </Link>
                 </Button>
-                <Button label={'YouTube'} href={'#'}>
+                <Button 
+                    label={'YouTube'} 
+                    boxSize={'35px'}
+                    _hover={{ 
+                        bg: 'linear-gradient(40deg, #FF0000, #FF0000, #FF0000, #FF0000, #FF0000, #FF0000)',
+                        color: 'white'
+                    }}
+                >
                     <Link to={'https://www.youtube.com/'} target='_blank'>
                         <FaYoutube />
                     </Link>
                 </Button>
-                <Button label={'Instagram'}>
+                <Button 
+                    label={'Instagram'} 
+                    _hover={{ 
+                        bg: 'linear-gradient(40deg, #405DE6 , #5851DB, #833AB4, #C13584,#E1306C, #FD1D1D)',
+                        color: 'white'
+                    }}
+                    boxSize={'35px'}
+                >
                     <Link to={'https://www.instagram.com/bereal.sf/'} target='_blank'>
                         <FaInstagram />
                     </Link>
