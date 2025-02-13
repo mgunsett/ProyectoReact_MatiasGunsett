d559896059034d4e6324a25ac797701f30e71b72
Matias Gunsett <matiasgunsett@gmail.com>
2025-02-13 00:28:03 -0300

    navbar burguer

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -6,107 +6,254 @@ import {
   MenuList,
   MenuItem,
   Button,
-  Icon
+  IconButton,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerBody,
+  useDisclosure,
+  useBreakpointValue,
+  Icon,
 } from "@chakra-ui/react";
-  import {CartWidget} from '../../components';
-  import logoBeReal from '../../assets/logo-BeReal.png';
-  import { Link } from "react-router-dom";
-  import { ChevronDownIcon, CloseIcon } from '@chakra-ui/icons';
-  import { useItemsCollection } from "../../hooks";
-  import { useEffect, useState } from "react";
-  import { useAuth } from "../../context/AuthContext";
-  import './Navbar.css';
+import { CartWidget } from "../../components";
+import logoBeReal from "../../assets/logo-BeReal.png";
+import { Link } from "react-router-dom";
+import { ChevronDownIcon, CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
+import { useItemsCollection } from "../../hooks";
+import { useEffect, useState } from "react";
+import { useAuth } from "../../context/AuthContext";
+import "./Navbar.css";
 
 export const Navbar = () => {
-    const { items } = useItemsCollection("categories");
-    const { user, logout } = useAuth();
+  const { items } = useItemsCollection("categories");
+  const { user, logout } = useAuth();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-    const [navClass, setNavClass] = useState("navbar");
+  const [navClass, setNavClass] = useState("navbar");
 
-  //Funcion para cambiar el color del navbar al hacer scroll
-    useEffect(() => {
-      const handleScroll = () => {
-        if (window.scrollY > 50) {
-          setNavClass("navbar scrolled");
-        } else {
-          setNavClass("navbar");
-        }
-      };
-      window.addEventListener("scroll", handleScroll);
-      return () => {
-        window.removeEventListener("scroll", handleScroll);
-      };
-    }, []);
-//------------------------------------------------------------//
-    return (
-    <>
-      <Box 
-       className={navClass}
-       px={4} 
-       position='sticky' 
-       top={0}  
-       zIndex={1}
-       >
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <Box display={'flex'} alignItems={'center'} gap={4}>
-            <Link to='/'>
-            <img width={'70px'} height={'70px'} src={logoBeReal} alt="Logo principal" />
-            </Link>
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setNavClass("navbar scrolled");
+      } else {
+        setNavClass("navbar");
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const isMobile = useBreakpointValue({ base: true, sm: true, md: false });
+
+  return (
+    <Box className={navClass} px={4} position="sticky" top={0} zIndex={10}>
+      <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+        {isMobile ? (
+          <IconButton
+            icon={<HamburgerIcon />}
+            colorScheme="whiteAlpha"
+            onClick={onOpen}
+            variant="outline"
+          />
+        ) : (
+          <Box display={"flex"} alignItems={"center"} gap={4}>
+            <Box flex={1} display="flex" justifyContent= "flex-start">
+              <Link to="/">
+                <img width={"70px"} height={"70px"} src={logoBeReal} alt="Logo principal" />
+              </Link>
+            </Box>
             <Menu>
-              <MenuButton className="menuButton"
-              as={Link} 
-              cursor="pointer"
-              color={'white'}
-              > Productos 
-                <ChevronDownIcon/>
+              <MenuButton className="menuButton" as={Link} color={"white"}>
+                Productos <ChevronDownIcon />
               </MenuButton>
-              <MenuList 
-                overflow={'visible'} 
-                backgroundColor={('gray.200', 'gray.700')} 
-                border={'1px solid white'}
-                boxShadow={'lg'}
-                transition={'all 0.3s'}
-              >
+              <MenuList backgroundColor={"gray.700"} border={"1px solid white"}>
                 {items.map((category) => (
-                  <MenuItem 
-                    key={category.slug} 
-                    backgroundColor={('gray.200', 'gray.700')}
-                  >
-                    <Link 
-                      to={`/category/${category.slug}`}
-                      className="menuButton2"
-                    >{category.name}
+                  <MenuItem key={category.slug} backgroundColor={"gray.700"}>
+                    <Link to={`/category/${category.slug}`} className="menuButton2">
+                      {category.name}
                     </Link>
                   </MenuItem>
                 ))}
-              </MenuList>   
+              </MenuList>
             </Menu>
-            <Link to='/contacto' className="menuButton">
+            <Link to="/contacto" className="menuButton">
               Contacto
-            </Link> 
+            </Link>
           </Box>
-          <Flex alignItems={'center'} justifyContent={'space-between'} gap={4}> 
+        )}
+        
+        <Box flex={1} display= {isMobile ? ('flex') : ('none')} justifyContent= "center">
+          <Link to="/">
+            <img width={"70px"} height={"70px"} src={logoBeReal} alt="Logo principal" />
+          </Link>
+        </Box>
+
+        {!isMobile && (
+          <Flex alignItems={"center"} gap={4}>
             {user && (
-              <Flex alignItems={'center'} justifyContent={'center'} gap={1}>
+              <Flex alignItems={"center"} gap={1}>
                 <Box color="white" mr={4}>
                   {user.displayName}
-                </Box>  
-                <Icon 
-                  boxSize={3}
-                  as={CloseIcon} 
-                  onClick={logout}
-                  color={'white'}
-                  _hover={{ 
-                    cursor: "pointer", 
-                    color: "red.500"
-                  }}
-                />
+                </Box>
+                <Icon boxSize={3} as={CloseIcon} onClick={logout} color={"white"} _hover={{ cursor: "pointer", color: "red.500" }} />
               </Flex>
             )}
-              <CartWidget />
+            <CartWidget />
           </Flex>
-        </Flex>
-      </Box>
-    </>
-    )
+        )}
+      </Flex>
+
+      {isMobile && (
+        <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+          <DrawerOverlay />
+          <DrawerContent backgroundColor="gray.800">
+            <DrawerCloseButton color="white" />
+            <DrawerBody>
+              <Menu>
+                <MenuButton as={Button} colorScheme="whiteAlpha" width="100%" mt={4}>
+                  Productos <ChevronDownIcon />
+                </MenuButton>
+                <MenuList backgroundColor={"gray.700"}>
+                  {items.map((category) => (
+                    <MenuItem key={category.slug} backgroundColor={"gray.700"}>
+                      <Link to={`/category/${category.slug}`} className="menuButton2">
+                        {category.name}
+                      </Link>
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </Menu>
+              <Link to="/contacto" className="menuButton" style={{ display: "block", marginTop: "10px" }}>
+                Contacto
+              </Link>
+              {user && (
+                <Flex alignItems={"center"} mt={4}>
+                  <Box color="white" mr={4}>
+                    {user.displayName}
+                  </Box>
+                  <Icon boxSize={3} as={CloseIcon} onClick={logout} color={"white"} _hover={{ cursor: "pointer", color: "red.500" }} />
+                </Flex>
+              )}
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      )}
+    </Box>
+  );
 };
+
+// import {
+//   Box,
+//   Flex,
+//   Menu,
+//   MenuButton,
+//   MenuList,
+//   MenuItem,
+//   Button,
+//   Icon
+// } from "@chakra-ui/react";
+//   import {CartWidget} from '../../components';
+//   import logoBeReal from '../../assets/logo-BeReal.png';
+//   import { Link } from "react-router-dom";
+//   import { ChevronDownIcon, CloseIcon } from '@chakra-ui/icons';
+//   import { useItemsCollection } from "../../hooks";
+//   import { useEffect, useState } from "react";
+//   import { useAuth } from "../../context/AuthContext";
+//   import './Navbar.css';
+
+// export const Navbar = () => {
+//     const { items } = useItemsCollection("categories");
+//     const { user, logout } = useAuth();
+
+//     const [navClass, setNavClass] = useState("navbar");
+
+//   //Funcion para cambiar el color del navbar al hacer scroll
+//     useEffect(() => {
+//       const handleScroll = () => {
+//         if (window.scrollY > 50) {
+//           setNavClass("navbar scrolled");
+//         } else {
+//           setNavClass("navbar");
+//         }
+//       };
+//       window.addEventListener("scroll", handleScroll);
+//       return () => {
+//         window.removeEventListener("scroll", handleScroll);
+//       };
+//     }, []);
+// //------------------------------------------------------------//
+//     return (
+//     <>
+//       <Box 
+//        className={navClass}
+//        px={4} 
+//        position='sticky' 
+//        top={0}  
+//        zIndex={1}
+//        >
+//         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+//           <Box display={'flex'} alignItems={'center'} gap={4}>
+//             <Link to='/'>
+//             <img width={'70px'} height={'70px'} src={logoBeReal} alt="Logo principal" />
+//             </Link>
+//             <Menu>
+//               <MenuButton className="menuButton"
+//               as={Link} 
+//               cursor="pointer"
+//               color={'white'}
+//               > Productos 
+//                 <ChevronDownIcon/>
+//               </MenuButton>
+//               <MenuList 
+//                 overflow={'visible'} 
+//                 backgroundColor={('gray.200', 'gray.700')} 
+//                 border={'1px solid white'}
+//                 boxShadow={'lg'}
+//                 transition={'all 0.3s'}
+//               >
+//                 {items.map((category) => (
+//                   <MenuItem 
+//                     key={category.slug} 
+//                     backgroundColor={('gray.200', 'gray.700')}
+//                   >
+//                     <Link 
+//                       to={`/category/${category.slug}`}
+//                       className="menuButton2"
+//                     >{category.name}
+//                     </Link>
+//                   </MenuItem>
+//                 ))}
+//               </MenuList>   
+//             </Menu>
+//             <Link to='/contacto' className="menuButton">
+//               Contacto
+//             </Link> 
+//           </Box>
+//           <Flex alignItems={'center'} justifyContent={'space-between'} gap={4}> 
+//             {user && (
+//               <Flex alignItems={'center'} justifyContent={'center'} gap={1}>
+//                 <Box color="white" mr={4}>
+//                   {user.displayName}
+//                 </Box>  
+//                 <Icon 
+//                   boxSize={3}
+//                   as={CloseIcon} 
+//                   onClick={logout}
+//                   color={'white'}
+//                   _hover={{ 
+//                     cursor: "pointer", 
+//                     color: "red.500"
+//                   }}
+//                 />
+//               </Flex>
+//             )}
+//               <CartWidget />
+//           </Flex>
+//         </Flex>
+//       </Box>
+//     </>
+//     )
+// };
