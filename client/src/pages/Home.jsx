b8b67e99da9e95eaf53ed641675a622e7803b137
Matias Gunsett <matiasgunsett@gmail.com>
2025-02-16 17:35:02 -0300

    hhover grid

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,9 +14,9 @@ import {
 } from "@chakra-ui/react";
 import logoAngel from "../assets/logoAngel.png";
 import fotoMain1 from '../assets/fotoMain1.jpg';
-import fotoMain2 from '../assets/fotoMain2.jpg';
+import fotoMain from '../assets/fotoMain.jpg';
 import fotoMain5 from '../assets/fotoMain5.jpg';
-import fotoMain4 from '../assets/fotoMain4.webp';
+import fotoMain6 from '../assets/fotoMain6.jpg';
 import fotoFamily from '../assets/fotoFamily.jpg';
 import tarjeta_cred from '../assets/tarjeta_cred.png';
 import envio_moto from '../assets/envio_moto.png';
@@ -39,7 +39,16 @@ export const Home = () => {
     }
   }, [ loading, items ]);
 // --------------------------------------------------------------------- //
-
+  
+// Estado para manejar Hover de las imagenes ---------------------------//
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+  const images = [
+    { src: fotoMain1, alt: 'Imagen 1'},
+    { src: fotoMain5, alt: 'Imagen 2'},
+    { src: fotoMain, alt: 'Imagen 3'},
+    { src: fotoMain6, alt: 'Imagen 4'},
+  ];
+// --------------------------------------------------------------------- //
   return loading ? (
     <SkeletonLoading />   
   ) : (
@@ -132,62 +141,36 @@ export const Home = () => {
         </Box>
       </Flex>
       <Grid
-        w={'85%'}
-        templateRows='repeat(2, 1fr)'
-        templateColumns='repeat(5, 1fr)'
-        gap={1}
-        margin={'auto'}
-      >
+      w={'85%'}
+      templateRows='repeat(2, 1fr)'
+      templateColumns='repeat(8, 1fr)'
+      gap={1}
+      margin={'auto'}
+    >
+      {images.map((image, index) => (
         <GridItem 
-          rowSpan={2} 
-          colSpan={2}
+          key={index}
+          onMouseEnter={() => setHoveredIndex(index)}
+          onMouseLeave={() => setHoveredIndex(null)}
+          rowSpan= {2}
+          colSpan={hoveredIndex === null ? 2 : hoveredIndex === index ? 4 : 1}
+          overflow={'hidden'}
           sx={{
-            transition: "all 0.3s ease-in-out",
-            _hover: { gridColumn: "span 4" }, // Expande la columna al pasar el mouse
-          }}
-        >
+            transitionProperty: 'all',
+            transitionDuration: '0.5s',
+            transitionTimingFunction: 'ease-in-out',
+          }}  
+        > 
           <Img
-          className="fotoMain1" 
-          src={fotoMain1}
-          objectFit={'cover'} 
-          height={'500px'}
-          width={'100%'} 
-          alt="imagen 1"
-          transition="all 0.3s ease-in-out"
-          _hover={{ transform: "scale(1.1)" }}
+            src={image.src}
+            objectFit={'cover'}
+            height={'500px'}
+            width={'100%'}
+            alt={image.alt}
           />
         </GridItem>
-        <GridItem rowSpan={2} colSpan={1}>
-          <Img 
-          className="fotoMain1"
-          src={fotoMain5}
-          objectFit={'cover'} 
-          height={'500px'}
-          width={'100%'} 
-          alt="imagen 2"
-          />
-        </GridItem>
-        <GridItem  rowSpan={1} colSpan={2} >
-          <Img 
-          className="fotoMain1"
-          src={fotoMain2} 
-          objectFit={'cover'}  
-          height={'250px'}
-          width={'100%'}
-          alt="imagen 3"
-          />
-        </GridItem>
-        <GridItem rowSpan={1} colSpan={2}>
-          <Img 
-          className="fotoMain1"
-          src={fotoMain4} 
-          objectFit={'cover'}
-          height={'250px'} 
-          width={'100%'}
-          alt="imagen 4"
-          />  
-        </GridItem>
-      </Grid>
+      ))}
+    </Grid>
       <Flex className="bannerInfo">
         <Box textAlign="center">
           <Img 
@@ -258,3 +241,61 @@ export const Home = () => {
     </Box>
   );
 };
+
+{/* <Grid
+        w={'85%'}
+        templateRows='repeat(2, 1fr)'
+        templateColumns='repeat(5, 1fr)'
+        gap={1}
+        margin={'auto'}
+      >
+        <GridItem 
+          rowSpan={2} 
+          colSpan={2}
+          sx={{
+            transition: "all 0.3s ease-in-out",
+            _hover: { gridColumn: "span 4" }, // Expande la columna al pasar el mouse
+          }}
+        >
+          <Img
+          className="fotoMain1" 
+          src={fotoMain1}
+          objectFit={'cover'} 
+          height={'500px'}
+          width={'100%'} 
+          alt="imagen 1"
+          transition="all 0.3s ease-in-out"
+          _hover={{ transform: "scale(1.1)" }}
+          />
+        </GridItem>
+        <GridItem rowSpan={2} colSpan={1}>
+          <Img 
+          className="fotoMain2"
+          src={fotoMain5}
+          objectFit={'cover'} 
+          height={'500px'}
+          width={'100%'} 
+          alt="imagen 2"
+          />
+        </GridItem>
+        <GridItem  rowSpan={1} colSpan={2} >
+          <Img 
+          className="fotoMain3"
+          src={fotoMain2} 
+          objectFit={'cover'}  
+          height={'250px'}
+          width={'100%'}
+          alt="imagen 3"
+          />
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={2}>
+          <Img 
+          className="fotoMain4"
+          src={fotoMain4} 
+          objectFit={'cover'}
+          height={'250px'} 
+          width={'100%'}
+          alt="imagen 4" 
+          />  
+        </GridItem>
+      </Grid> */}
