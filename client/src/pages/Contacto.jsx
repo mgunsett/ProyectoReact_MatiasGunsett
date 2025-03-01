36a62b1f99719e8009ea7669bafe9425e142800f
Matias Gunsett <matiasgunsett@gmail.com>
2025-03-01 00:00:42 -0300

    finish respons

--- client/src/pages/Contacto.jsx
+++ client/src/pages/Contacto.jsx
@@ -43,7 +43,7 @@ export const Contacto = () => {
       >
         <Flex
           className='contacto-container'
-          maxW={{base:'90%', sm:'80%', md:'60%'}}
+          w={{ base: '90%', sm: '90%', md: '60%'}}
           margin={'60px auto'}
           p={'50px'}
           direction={{ base: 'column', sm: 'column', md: 'row', lg: 'row' }}
@@ -51,60 +51,67 @@ export const Contacto = () => {
           justifyContent={{ base: 'center', sm: 'center', md: 'space-evenly'}}
         >
           <Box>
-            <Heading
-              fontSize={{ base: '2xl', sm: '4xl', md: '4xl', lg: '5xl' }}
-              color="white"
-              fontFamily={'"Lacquer", system-ui, sans-serif'}
-            >Contacto
-            </Heading>
-            <Text mt={{ sm: 3, md: 3, lg: 5 }} color="white">
-              Nuestros medios de Contacto:
-            </Text>
             <Flex
-              flexDirection={'column'}
-              alignItems={'flex-start'}
-              gap={6}
+              direction="column"
+              alignItems={{ base: 'center', sm: 'flex-start' }}
+              gap={4}
+            >
+              <Heading
+                fontSize={{ base: '37px', sm: '4xl', md: '4xl', lg: '5xl' }}
+                color={"rgba(237, 237, 78, 0.737)"}
+                fontFamily={'"Lacquer", system-ui, sans-serif'}
+              >
+                Contacto
+              </Heading>
+              <Text 
+                mt={{ sm: 4, md: 3, lg: 5 }} 
+                color="white"
+                borderBottom={'1px solid #DCE2FF'}
+                display={{base: 'none', sm: 'block'}}
+              >
+                Nuestros medios de Contacto
+              </Text>
+            </Flex>
+            <Flex
+              direction="column"
+              alignItems="flex-start"
+              gap={{ base: 1, sm: 4 }}
               paddingTop={5}
+              marginLeft={{base:'-15px'}}
             >
               <Button
                 size={{ base: 'md', sm: 'lg' }}
-                variant="ghost"
-                color="#DCE2FF"
-                _hover={{ border: '2px solid #1C6FEB' }}
-                leftIcon={
-                  <MdPhone
-                    color="rgb(240, 240, 42)"
-                    size="20px"
-                  />
-                }
+                colorScheme='Gray'
+                color=" #DCE2FF"
+                marginLeft={'-2px'}
+                leftIcon={<MdPhone color="rgb(240, 240, 42)" size="20px"/>}
               >
                 +54-342-111-1111
               </Button>
-              <Button
-                mr={5}
+              <Button 
                 size="md"
-                variant="ghost"
+                colorScheme='Gray'
                 color="#DCE2FF"
-                _hover={{ border: '2px solid #1C6FEB' }}
-                leftIcon={<MdEmail color="rgb(240, 240, 42)" size="20px" />}>
+                leftIcon={<MdEmail color="rgb(240, 240, 42)" size="20px" />}
+              >
                 be-realclothes@gmail.com
               </Button>
               <Button
                 size="md"
-
-                variant="ghost"
+                colorScheme='Gray'
                 color="#DCE2FF"
-                _hover={{ border: '2px solid #1C6FEB' }}
                 leftIcon={<MdLocationOn color="rgb(240, 240, 42)" size="20px" />}>
                 Santa Fe, Argentina
               </Button>
             </Flex>
 
             <HStack
-              mt={{ lg: 10, md: 8, sm: 6 }}
+              mt={{ lg: 10, md: 8, base: 6 }}
+              mb={{base: 10}}
               spacing={5}
               px={5}
-              alignItems="flex-start">
+              alignItems="flex-start"
+            >
               <Link to='https://www.facebook.com/' target='_blank'>
                 <IconButton
                   color='white'
@@ -142,12 +149,11 @@ export const Contacto = () => {
           </Box>
           <Box
             bg="white"
-            borderRadius="lg"
-            boxShadow="2xl"
-            maxW={{ base: '90%', sm: '100%' }}
+            p={7}
+            w={{ base: '280px', sm: '50%' }}
+            h={'auto'}
           >
-            <Box m={8} color="#0B0E3F">
-              <VStack spacing={5}>
+              <VStack spacing={3}>
                 <FormControl id="name">
                   <FormLabel>Nombre</FormLabel>
                   <InputGroup borderColor="#E0E1E7">
@@ -182,7 +188,6 @@ export const Contacto = () => {
                   </Button>
                 </FormControl>
               </VStack>
-            </Box>
           </Box> 
         </Flex>
       </Box>
